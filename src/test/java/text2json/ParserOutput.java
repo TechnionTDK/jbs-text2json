348e77e0447c6cd78a9747de853259c5f08adac9
Oren Mishali <oren.mishali@gmail.com>
2017-01-18 00:20:11 +0200

    improved test for shmonakvatzim

---
+++ src/test/java/text2json/ParserOutput.java
@@ -0,0 +1,11 @@
+package text2json;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by omishali on 17/01/2017.
+ */
+public class ParserOutput {
+    List<Object> subjects = new ArrayList<Object>();
+}
--- src/test/java/text2json/ShmonaKvatzimParserTest.java
+++ src/test/java/text2json/ShmonaKvatzimParserTest.java
@@ -1,5 +1,6 @@
 package text2json;
 
+import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
@@ -7,23 +8,27 @@ import org.junit.Test;
 import text2json.parsers.ShmonaKvatzimParser;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 
 /**
  * Created by omishali on 17/01/2017.
  */
 public class ShmonaKvatzimParserTest {
-    private static JsonReader jsonReader;
+    static ParserOutput output;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
         Parser parser = new ShmonaKvatzimParser();
         BufferedReader reader = TestUtils.getText("shmonakvatzim/shmonakvatzim.txt");
         parser.parse(reader, "json/shmonakvatzim/shmonakvatzim.json");
-        jsonReader = TestUtils.getJson("shmonakvatzim/shmonakvatzim.json");
+        Gson gson = new Gson();
+        output = gson.fromJson(new BufferedReader(new FileReader("json/shmonakvatzim/shmonakvatzim.json")), ParserOutput.class);
+        //jsonReader = TestUtils.getJson("shmonakvatzim/shmonakvatzim.json");
     }
 
     @Test
     public void testTotalNumberOfElements() {
-        assertNotNull(jsonReader);
+        assertNotNull(output);
+        assertEquals(2822, output.subjects.size());
     }
 }
