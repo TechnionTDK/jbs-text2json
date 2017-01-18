639bbec26d2e7f14dffbbd0553f30a46d207c414
Oren Mishali <oren.mishali@gmail.com>
2017-01-18 23:14:33 +0200

    added parser and test for seferhamitzvot

---
+++ src/main/java/text2json/parsers/SeferHamitzvotParser.java
@@ -0,0 +1,95 @@
+package text2json.parsers;
+
+import text2json.Line;
+import text2json.LineMatcher;
+import text2json.Parser;
+
+import java.io.IOException;
+
+import static text2json.JbsOntology.*;
+
+/**
+ * Created by omishali on 18/01/2017.
+ */
+public class SeferHamitzvotParser extends Parser {
+    private static final String BEGIN_MITZVAT_ASE = "begin_ase";
+    private static final String BEGIN_MITZVAT_LOTAASE = "begin_lotaase";
+    private static final String BEGIN_MITZVA = "begin_mitzva";
+    private static final String SEFER_HAMITZVOT = "ספר המצוות";
+
+    private int sectionNum;
+    private int mitzvaNum;
+    private String mitzvaType;
+
+
+    @Override
+    protected void registerMatchers() {
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_MITZVAT_ASE;
+            }
+
+            @Override
+            public boolean match(Line line) {
+                return line.is("מצות עשה");
+            }
+        });
+
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_MITZVAT_LOTAASE;
+            }
+
+            @Override
+            public boolean match(Line line) {
+                return line.is("מצות לא תעשה");
+            }
+        });
+
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_MITZVA;
+            }
+
+            @Override
+            public boolean match(Line line) {
+                return line.beginsWith("מצוה") && line.wordCount() == 2;
+            }
+        });
+    }
+
+    @Override
+    protected void onLineMatch(String type, Line line) throws IOException {
+        switch(type) {
+            case BEGIN_MITZVAT_ASE:
+                sectionNum = 3; // hakdama is section 1, shorshei hamitzvot section 2
+                mitzvaNum = 0;
+                mitzvaType = "עשה";
+                break;
+            case BEGIN_MITZVAT_LOTAASE:
+                sectionNum = 4;
+                mitzvaNum = 0;
+                mitzvaType = "לא תעשה";
+                break;
+            case BEGIN_MITZVA:
+                jsonObjectFlush();
+                mitzvaNum++;
+                jsonObjectAdd(URI, getUri());
+                jsonObjectAdd(RDFS_LABEL, SEFER_HAMITZVOT + " " + mitzvaType + " " + line.getLine());
+                jsonObjectAdd(JBO_SEFER, JBR + "seferhamitzvot");
+                break;
+            case NO_MATCH:
+                jsonObjectAppend(JBO_TEXT, line.getLine());
+                break;
+        }
+
+    }
+
+    @Override
+    protected String getUri() {
+        return JBR + "seferhamitzvot-" + sectionNum + "-" + mitzvaNum;
+    }
+}
---
+++ src/test/java/text2json/SeferHamitzvotParserTest.java
@@ -0,0 +1,52 @@
+package text2json;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import text2json.parsers.SeferHamitzvotParser;
+
+import java.io.BufferedReader;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static text2json.TestUtils.getJson;
+import static text2json.TestUtils.getText;
+import static text2json.JbsOntology.*;
+
+/**
+ * Created by omishali on 18/01/2017.
+ */
+public class SeferHamitzvotParserTest {
+    static SubjectsJson json;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Parser parser = new SeferHamitzvotParser();
+        BufferedReader reader = getText("seferhamitzvot/seferhamitzvot.txt");
+        parser.parse(reader, "json/seferhamitzvot/seferhamitzvot.json");
+        json = getJson("json/seferhamitzvot/seferhamitzvot.json");
+    }
+
+    @Test
+    public void testTotalNumberOfObjects() {
+        assertNotNull(json);
+        assertEquals(613, json.subjects.size());
+    }
+
+    @Test
+    public void testObjects() {
+        // test first object
+        Map<String, String> o = json.getObject(0);
+        assertEquals("jbr:seferhamitzvot-3-1", o.get(URI));
+        assertEquals("ספר המצוות עשה מצוה א.", o.get(RDFS_LABEL));
+        assertEquals("jbr:seferhamitzvot", o.get(JBO_SEFER));
+        assertTrue(o.get(JBO_TEXT).startsWith("היא הצווי אשר צונו בהאמנת"));
+
+        // test last object
+        o = json.getObject(612);
+        assertEquals("jbr:seferhamitzvot-4-365", o.get(URI));
+        assertEquals("ספר המצוות לא תעשה מצוה שסה:", o.get(RDFS_LABEL));
+        assertEquals("jbr:seferhamitzvot", o.get(JBO_SEFER));
+        assertTrue(o.get(JBO_TEXT).startsWith("הזהיר המלך מהרבות ממון מיוחד לעצמו"));
+    }
+}
