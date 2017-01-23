957998cf67e6890abeafe0ec14f781a1892e951a
Oren Mishali <oren.mishali@gmail.com>
2017-01-23 14:32:25 +0200

    added method stripVowels to TestUtils

--- src/test/java/text2json/TestUtils.java
+++ src/test/java/text2json/TestUtils.java
@@ -19,13 +19,23 @@ public class TestUtils {
         return new BufferedReader(getFileReader(TEXT_DIR + s));
     }
 
-    static FileReader getFileReader(String s) throws FileNotFoundException {
+    public static FileReader getFileReader(String s) throws FileNotFoundException {
         String filePath = new File("").getAbsolutePath();
         return new FileReader(filePath.concat(s));
     }
 
-    static SubjectsJson getJson(String json) throws Exception {
+    public static SubjectsJson getJson(String json) throws Exception {
         Gson gson = new Gson();
         return gson.fromJson(new BufferedReader(new FileReader(json)), SubjectsJson.class);
+    }
+
+    public static String stripVowels(String rawString){
+        String newString = "";
+        for(int j=0; j<rawString.length(); j++) {
+            if(rawString.codePointAt(j)<1425
+                    || rawString.codePointAt(j)>1479)
+            { newString = newString + rawString.charAt(j); }
+        }
+        return(newString);
     }
 }
