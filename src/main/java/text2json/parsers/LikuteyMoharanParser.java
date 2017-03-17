1a9e5a9a9ea3c7a3b8431885e1e4f9a5d9fd8322
Igal Brenner <igalbrenner@gmail.com>
2017-03-17 12:58:28 +0200

    Likutey moharan parser + tests (issue #36)
    Signed-off-by: Igal Brenner <igalbrenner@gmail.com>

---
+++ src/main/java/text2json/parsers/LikuteyMoharanParser.java
@@ -0,0 +1,129 @@
+package text2json.parsers;
+
+import text2json.Line;
+import text2json.LineMatcher;
+import text2json.Parser;
+import static text2json.JbsOntology.*;
+import java.io.IOException;
+import static text2json.JbsUtils.*;
+
+/**
+ * Created by USER on 16-Mar-17.
+ */
+public class LikuteyMoharanParser extends Parser{
+    protected static final String BEGIN_CHELEK = "begin_chelek";
+    protected static final String BEGIN_TEXT = "begin_text";
+
+    //TODO: add to JbsOntology?
+    public static String JBO_POSITION_IN_CHELEK = "jbo:positionInChelek";
+    //
+
+    private int chelekNum = 0;
+    private int saifNum = 0;
+    private int positionInSefer = 0;
+    private String chelekHebIdx;
+    private String saifTitle;
+    private String saifHebIdx;
+
+    public LikuteyMoharanParser() { createPackagesJson(); }
+
+    @Override
+    protected void registerMatchers() {
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_SEFER;
+            }
+            @Override
+            public boolean match(Line line) {
+                return line.beginsWith("ליקוטי מוהר''ן") && line.wordCount() == 2;
+            }
+        });
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_CHELEK;
+            }
+            @Override
+            public boolean match(Line line) {
+                return line.beginsWith("ליקוטי מוהר''ן - חלק ") && line.wordCount() == 5;
+            }
+        });
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_SAIF;
+            }
+            @Override
+            public boolean match(Line line) {
+                if (!line.contains(" -")) return false;
+                String hebIdx = line.extract(" ", " - ");
+                return !hebIdx.contains(".") && hebIdx.length() <= 3 && line.wordCount() <= 15;
+            }
+        });
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_TEXT;
+            }
+            @Override
+            public boolean match(Line line) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onLineMatch(String type, Line line) throws IOException {
+        switch (type) {
+            case BEGIN_SEFER:
+                break;
+
+            case BEGIN_CHELEK:
+                jsonObjectFlush();
+                saifNum = 0;
+                chelekNum++;
+                chelekHebIdx = line.extract("ליקוטי מוהר''ן - חלק ", " ");
+                //add chelek to package json
+                packagesJsonObject().add(URI, "jbr:likuteymoharan-" + chelekNum);
+                packagesJsonObject().add(RDFS_LABEL, "ליקוטי מוהר''ן - חלק " + chelekHebIdx);
+                packagesJsonObject().add(JBO_POSITION, chelekNum);
+                packagesJsonObjectFlush();
+                break;
+
+            case BEGIN_SAIF:
+                jsonObjectFlush();
+                saifNum++;
+                if ((chelekNum == 1 && saifNum == 215) || (chelekNum == 2 && saifNum == 102)) saifNum++;
+                if (chelekNum == 1 && saifNum == 270) saifNum+=2;
+                positionInSefer++;
+                saifHebIdx = line.getFirstWord();
+                saifTitle = line.extract(" - ", " ");
+                jsonObject().add(URI, getUri());
+                jsonObject().add(JBO_SEFER, "likuteymoharan");
+                jsonObject().add(JBO_POSITION_IN_CHELEK, saifNum);
+                jsonObject().add(JBO_POSITION, positionInSefer);
+                jsonObject().add(RDFS_LABEL, "חלק " + chelekHebIdx + " סעיף " + saifHebIdx + " - " + saifTitle);
+                //add saif to package json
+                packagesJsonObject().add(URI, getUri());
+                packagesJsonObject().add(RDFS_LABEL, saifTitle);
+                packagesJsonObject().add(JBO_POSITION, positionInSefer);
+                packagesJsonObjectFlush();
+                break;
+
+            case BEGIN_TEXT:
+                jsonObject().append(JBO_TEXT, line.getLine());
+                break;
+
+            case NO_MATCH:
+                onLineMatch(BEGIN_TEXT, line);
+                break;
+        }
+    }
+
+    @Override
+    protected String getUri() {
+        return "jbr:likuteymoharan-" + chelekNum + "-" + saifNum;
+    }
+
+}
