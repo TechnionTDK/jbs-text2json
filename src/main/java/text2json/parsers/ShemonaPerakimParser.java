eb14d2d33825e5b1e167a96c71acbcb9fa914eff
Orel <orelkanditan@gmail.com>
2018-07-22 18:14:26 +0300

    wrote ShemonaPerkaimParser and ShemonaPerkaimParserTest

---
+++ src/main/java/text2json/parsers/ShemonaPerakimParser.java
@@ -0,0 +1,92 @@
+package text2json.parsers;
+
+import text2json.JbsParser;
+import text2json.Line;
+import text2json.LineMatcher;
+
+import java.io.IOException;
+
+/**
+ * Created by orel on 22/07/18.
+ */
+public class ShemonaPerakimParser extends JbsParser {
+
+    private int perekNum = 0;
+    @Override
+    protected void registerMatchers() {
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_SEFER;
+            }
+
+            @Override
+            public boolean match(Line line) {
+                return line.beginsWith("שמונה פרקים לרמב\"ם");
+            }
+        });
+
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_HAKDAMA;
+            }
+
+            @Override
+            public boolean match(Line line) {
+                return line.beginsWith("הקדמה") && line.wordCount() == 1;
+            }
+        });
+
+        registerMatcher(new LineMatcher() {
+            @Override
+            public String type() {
+                return BEGIN_PEREK;
+            }
+
+            @Override
+            public boolean match(Line line) {
+                return line.beginsWith("Chapter") && line.wordCount() == 2;
+            }
+        });
+    }
+
+    @Override
+    protected void onLineMatch(String type, Line line) throws IOException {
+        switch(type) {
+            case BEGIN_SEFER:
+                break;
+
+            case BEGIN_PEREK:
+                jsonObjectFlush();
+                perekNum++;
+                addBook(jsonObject(), getBookId());
+                addUri(getBookId()+"-" + perekNum);
+                addPosition(jsonObject(), perekNum);
+                addLabel(jsonObject(), "שמונה פרקים לרמב\"ם פרק " + perekNum);
+                break;
+
+            case BEGIN_HAKDAMA:
+                jsonObjectFlush();
+                addBook(getBookId());
+                addUri(getBookId()+"-0");
+                addPosition(0);
+                addLabel("שמונה פרקים לרמב\"ם הקדמה");
+                break;
+
+            case NO_MATCH:
+                appendText( line.getLine());
+                break;
+        }
+    }
+
+    @Override
+    protected String getUri() {
+        return null;
+    }
+
+    @Override
+    protected String getBookId() {
+        return "shemonaperakim";
+    }
+}
